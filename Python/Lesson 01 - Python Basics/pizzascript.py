911914867e0f944a9d8cbf4f561b1ec9e2b644c3
Matt <Matt@matt.com>
2026-07-10 15:29:10 -0400

    Pizza Script

---
+++ Python/Lesson 01 - Python Basics/pizzascript.py
@@ -0,0 +1,17 @@
+print ("This will help you determine which pizza is better so you can be the fatboy you want to be for the least money!")
+small = int(input ("What size is the Small Pizza?  "))
+smallnumber = int(input("How many Small Pizzas?  "))
+smallprice = float(input ("How much are the small Pizzas?  "))
+large = int(input ("What size is the Large Pizza?  "))
+largenumber = int(input("How many Large Pizzas?"))
+largeprice = float(input ("How much are the Large Pizzas?"))
+smallx = (3.14 * (small/2) ** 2.0) * smallnumber
+largex = (3.14 * (large/2) ** 2.0) * largenumber
+print(f"Small is equal to {smallx} square inches of pizza. The cost per square inch is {largex / largeprice:.02f}")
+print(f"Large is equal to {largex} square inches of pizza. The cost per square inch is {smallx / smallprice:.02f}")
+if smallx > largex:
+    print ("Small is better than large.")
+elif smallx == largex:
+    print ("Small is the same as large.")
+else: 
+    print ("Large is better than small.")
